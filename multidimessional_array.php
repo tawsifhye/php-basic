e3e84807b5eefb11725bd9e595de956ea5d720b4
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-07-20 09:54:31 +0600

    first commit

---
+++ multidimessional_array.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+
+<?php
+    $information = array(
+        array('Gloria',	'Reeves',67439,'10/18/1985',4,0.1,50),
+        array('Graham','Bonner',90727,'12/07/1983',4,0.9,90),
+        array('Warren','Wheeler',99134,'11/11/1984',2,0.7,50),
+        array('Zena','Hale',19803,	'06/17/1987',9,0.3,90),
+        array('Julia',	'Haupt',	24116,	'03/15/1991',10,1.0,40),
+    );
+echo '<table>';
+    echo '
+    <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>ZIP</th>
+    <th>Birthday</th>
+    </tr>
+    ';
+    echo '</table>';
+?>
