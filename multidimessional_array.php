1d2d314c1fbfbf64cc03afeb852256e00056b0b9
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-07-20 10:52:30 +0600

    data added

--- multidimessional_array.php
+++ multidimessional_array.php
@@ -2,20 +2,31 @@
 
 <?php
     $information = array(
-        array('Gloria',	'Reeves',67439,'10/18/1985',4,0.1,50),
-        array('Graham','Bonner',90727,'12/07/1983',4,0.9,90),
-        array('Warren','Wheeler',99134,'11/11/1984',2,0.7,50),
-        array('Zena','Hale',19803,	'06/17/1987',9,0.3,90),
-        array('Julia',	'Haupt',	24116,	'03/15/1991',10,1.0,40),
+        array('Gloria',	'Reeves',67439,'10/18/1985'),
+        array('Graham','Bonner',90727,'12/07/1983'),
+        array('Warren','Wheeler',99134,'11/11/1984'),
+        array('Zena','Hale',19803,	'06/17/1987'),
+        array('Julia','Haupt',24116,'03/15/1991'),
+        array('Julias','Agens',241166,'03/15/1998'),
     );
+    echo "<div class='table_container'>";
 echo '<table>';
-    echo '
+    echo "
     <tr>
     <th>First Name</th>
     <th>Last Name</th>
     <th>ZIP</th>
     <th>Birthday</th>
     </tr>
-    ';
+    ";
+
+    for($row =0; $row< sizeof($information); $row++ ){
+  echo "<tr class='table_data'>";
+    for($col=0; $col <sizeof($information[$col]); $col++){
+        echo '<td>'.$information[$row][$col].'</td>';
+    }
+echo "</tr>";
+}
     echo '</table>';
+    echo "</div>";
 ?>
